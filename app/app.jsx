ed99d15300012f3b5a55470b9e8f4e7c64ae3057
Joonas Tuominen <jomppe.tuominen@gmail.com>
2017-01-27 21:17:33 +0200

    Added foundation & jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,9 @@ var Weather = require('weather');
 var About = require('about');
 var Examples = require('examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 // initialize the app
 ReactDOM.render(
   <Router history={hashHistory}>
